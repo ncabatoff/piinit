3bcef148ae00de72e0dcc233ed897b46471a5612
Nick Cabatoff <ncabatoff@hashicorp.com>
2019-02-10 14:31:29 -0500

    Add Prometheus.

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 set -e
 WORKDIR=packer-builder-arm-image
 cp ~/.ssh/id_rsa.pub $WORKDIR/authorized_keys
-cp -a *.json init-consul init-nomad {install,run}-node_exporter $WORKDIR
+cp -a *.json init-consul init-nomad prometheus node_exporter $WORKDIR
 cd $WORKDIR
 PACKERFILE=packer.json vagrant provision --provision-with build-image
